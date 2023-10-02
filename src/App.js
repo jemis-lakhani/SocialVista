5cf9c3c007de89da35ce7e5cd57bc1d3c9ad99ae
jemislakhani <jemislakhani.tech@gmail.com>
2023-10-03 01:13:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path="/home" element={<HomePage />}></Route>
+        <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate
+          loading={null}
+          persistor={persistStore(store)}
+        ></PersistGate>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>
+);
